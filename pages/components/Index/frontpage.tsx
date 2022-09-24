44daa87cbb4255f96e9486b7ee4f39076cee7a45
Leo <leonardomrel0028@gmail.com>
2022-09-24 19:27:27 +0200

    added tailwind

--- pages/components/Index/frontpage.tsx
+++ pages/components/Index/frontpage.tsx
@@ -3,7 +3,7 @@ import { NextPage } from "next";
 const FrontPage: NextPage = () => {
     return (
         <div>
-            <h5>FrontPage</h5>
+            <h5 className="text-red-600">FrontPage</h5>
         </div>
     )
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import FrontPage from './components/frontpage'
+import FrontPage from './components/Index/frontpage'
 
 const Home: NextPage = () => {
   return (
