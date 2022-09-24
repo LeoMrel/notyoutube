257e18ea5bab6d3eaf166eb841fa23e165dd7e95
Leo <leonardomrel0028@gmail.com>
2022-09-24 19:24:12 +0200

    working on frontpage

---
+++ pages/components/frontpage.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next";
+
+const FrontPage: NextPage = () => {
+    return (
+        <div>
+            <h5>FrontPage</h5>
+        </div>
+    )
+};
+
+export default FrontPage
